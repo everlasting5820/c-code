29b31c0b7d14011631b8b1a6fa14796d5b48dccc
everlasting <52740438+everlasting5820@users.noreply.github.com>
2020-07-31 22:36:33 +0800

    C语言【1到100各种数和】

--- 6.24.cpp
+++ 6.24.cpp
@@ -1,21 +1,36 @@
 # include <stdio.h>
-
 int main(void)
 {
-	int val;
-	int sum=0;
-	printf("请输入要判断的数：")；
-	scanf("%d",&val);
-
-	m = val;
-	while (m)
+	int i;
+	int sum = 0;
+	int sum_= 0;
+	int cnt = 0;
+	int cnt_= 0;
+	float avg,avg_;
+	for(i=1; i<=100; i+=1)
 	{
-		sum = sum*10 + m%10;
-		m /= 10;
+		if(i%2==0)
+		{
+			sum = sum + i;
+			++cnt;
+		}
+		else if(i%2==1)
+		{
+			sum_=sum_+ i;
+			++cnt_;
+		}
+
 	}
-	if(sum==val)
-		printf("yes\n");
-	else
-		printf("no\n");
+	avg = sum/cnt;
+	avg_= sum_/cnt_;
+		//sum = sum +i;
+	printf("i = %d\n",i);
+	printf("sum = %d\n",sum);
+	printf("sum_= %d\n",sum_);
+	printf("cnt = %d\n",cnt);
+	printf("cnt_ = %d\n",cnt_);
+	printf("avg = %f\n",avg);
+	printf("avg_= %f\n",avg_);
+	
 	return 0;
 }
