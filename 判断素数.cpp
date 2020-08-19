128b577a4ae2dc93abb0e63df6c4e5a55d0445c6
everlasting <52740438+everlasting5820@users.noreply.github.com>
2020-08-19 23:16:33 +0800

    Update 判断素数.cpp

--- 判断素数.cpp
+++ 判断素数.cpp
@@ -11,12 +11,13 @@ int main()
 	for(i=2; i<x; i++){
 		if(x % i == 0){
 			isPrime = 0;
+                        break;
 		}
 	}
 	if(isPrime == 1){
-		printf("是素数\n");
+		printf("脢脟脣脴脢媒\n");
 	} else {
-		printf("不是素数\n");
+		printf("虏禄脢脟脣脴脢媒\n");
 	}
 	return 0;
 }
